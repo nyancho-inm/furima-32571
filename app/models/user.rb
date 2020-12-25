7805fbce7c573ddbb23cbfabee47bd9a0529ff23
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2020-12-26 01:00:08 +0900

    userモデル単体テスト実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  VALID_PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "Include both letters and numbers" } 
+
   with_options presence: true do
     validates :nickname
     validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :first_name_kana, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :last_name_kana, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Full-width characters"}
     validates :birthday
   end
 end
