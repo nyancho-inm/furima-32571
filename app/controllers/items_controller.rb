cabf7e39cd7e48fd7a74364d4d0c9600b89b15f8
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2020-12-27 17:03:09 +0900

    ActiveHashの内容をビューファイルに反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item  = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :image, :category_id, :condition_id, :fee_id, :area_id, :day_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :fee
   belongs_to_active_hash :area
-  belongs_to_active_hash :days
+  belongs_to_active_hash :day
 
 
   with_options presence: true do
     validates :item_name
     validates :description
+    validates :image
     validates :category_id, numericality: { other_than: 1 }
     validates :condition_id, numericality: { other_than: 1 }
     validates :fee_id, numericality: { other_than: 1 }
     validates :area_id, numericality: { other_than: 1 }
     validates :day_id, numericality: { other_than: 1 }
-    validates :price, format: {with: 	/^[0-9]*$/}, length: {minimum: 300, maximum: 9,999,999}
+    validates :price, format: {with: /\A[a-zA-Z0-9]+\z/}, length: {minimum: 300, maximum: 9999999}
   end
 end
