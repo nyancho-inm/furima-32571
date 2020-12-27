3ba78a7ac1d356b7784b4fc7049028508529f414
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2020-12-27 14:30:47 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :fee_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :day_id, numericality: { other_than: 1 }
+    validates :price, format: {with: 	/^[0-9]*$/}, length: {minimum: 300, maximum: 9,999,999}
+  end
 end
