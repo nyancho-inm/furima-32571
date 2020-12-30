b28c1215cd2da4f20bd4c9eec0d84ec957726ee2
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2020-12-30 11:45:40 +0900

    出品者意外が直接編集ページへ遷移できないよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -26,9 +27,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :description, :image, :category_id, :condition_id, :fee_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    redirect_to(root_path) unless @item == current_user
   end
 end
