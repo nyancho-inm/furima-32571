bf1677ff784c330b01025152cfb2f0df2027e702
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-06 18:38:27 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # get 'orders/index'
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :orders
+    resources :orders, only: [:index, :create]
   end
 end
